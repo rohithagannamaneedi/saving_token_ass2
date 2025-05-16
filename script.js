9d28494bac789a06d0c789932d42e36eacd97e10
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-05-16 10:47:41 +0530

    ass2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const jwt = require('jsonwebtoken');
+
+const encrypt = (payload, secret) => {
+  // Generate a JWT with 1 hour expiry
+  return jwt.sign(payload, secret, { expiresIn: '1h' });
+};
+
+
 };
 
 module.exports = encrypt;
